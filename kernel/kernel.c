f575c65c5fd952bfb823b30a6b83a3def6f20dd8
AlexanderFrancoletti <alexander.francoletti@gmail.com>
2018-11-16 01:41:25 -0500

    Added basic string printing functionality and function to get string length

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,6 +3,52 @@ void dummy_test_entrypoint() {
 }
 
 void main() {
-	char* video_memory = (char*) 0xb8000;
-	*video_memory = 'X';
+	// char* video_memory = (char*) 0xb8000;
+	// *video_memory = '1';
+	// char* video_mem2 = (char*) 0xb80a0;
+	// *video_mem2 = '2';
+	// char* video_mem3 = (char*) 0xb8100;
+	// char* video_mem4 = (char*) 0xb8150;
+	// char* video_mem5 = (char*) 0xb8200;
+	// char* video_mem6 = (char*) 0xb8250;
+	// char* video_mem7 = (char*) 0xb8300;
+	// char* video_mem8 = (char*) 0xb8350;
+	// char* video_mem9 = (char*) 0xb8400;
+	// char* video_mem0 = (char*) 0xb8450;
+	// char* video_mema = (char*) 0xb8500;
+	// char* video_memb = (char*) 0xb8550;
+	// *video_mem3 = '3';
+	// *video_mem4 = '4';
+	// *video_mem5 = '5';
+	// *video_mem6 = '6';
+	// *video_mem7 = '7';
+	// *video_mem8 = '8';
+	// *video_mem9 = '9';
+	// *video_mem0 = '0';
+	// *video_mema = 'A';	
+	// *video_memb = 'B';
+
+	print_custom("BASA-OS loaded\0", 0xb8a00);
+
+	//Not to self - memory+500 is in the same column
+	//Each row contains a0 characters
+
+
+}
+
+void print_custom(char* str, int pos)
+{
+	for (int x = 0; x < length(str); ++x)
+	{
+		char* temp_mem = (char*) pos + 0x2*x;
+		*temp_mem = str[x];
+	}
+}
+
+int length(char* str)
+{
+	int x = 0;
+	while (str[x] != '\0')
+		x++;
+	return x;
 }
